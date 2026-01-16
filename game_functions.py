dc2862bcbe30239ad961f66904d4ad330c6750b3
Jack Tomes <jst218@exeter.ac.uk>
2026-01-16 12:25:42 +0000

    game 2 complete

--- game_functions.py
+++ game_functions.py
@@ -7,7 +7,15 @@ def pick_value(poss_values):
 
 # function to be used in game_2: Higher or Lower
 def check_higher_lower(current_val, next_val, user_input):
-    pass
+    if next_val == current_val:
+        next_val = random.randint(min_x,max_x)
+    else:
+        if current_val > next_val:
+            if user_input == 'l':
+                return True
+        else:
+            if user_input == 'h':
+                return True
 
 # function to be used in game_3: Hangman
 def process_guess(letter, board, word):
